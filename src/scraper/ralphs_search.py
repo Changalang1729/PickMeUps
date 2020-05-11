18ca64a01906ad38f4c109d171e9ea458f4951a8
Vikas Munukutla <vikas9.m@gmail.com>
2020-05-10 17:54:30 -0700

    added comments to ralphs_search.py

--- src/scraper/ralphs_search.py
+++ src/scraper/ralphs_search.py
@@ -9,8 +9,12 @@ ZIP_CODE = '92602'
 # 'Milk', 'Eggs', 'Paper towels', 'Bread', 'Flour', 'Toilet paper', 'Chips', 'fruits', 'Soap', 'Body wash', 'Shampoo', 'Pasta', 'Rice', 'Water', 'Meat','Wine','Thermometer', 'Cough medicine', 'vegetables', 'Tylenol', 'Advil', 'Medicine'
 # didn't work: 'Medicine',
 SEARCH_ITEMS = []
+# maximum distance from ZIP_CODE
 DISTANCE_THRESHOLD = 7
 
+# Get relevant product information from each product and put it
+# in the csv named by store_name. If first is True, write the csv
+# header along with the product info.
 def processProduct(product, store_name, item, first):
     name = ''
     size = ''
@@ -28,31 +32,28 @@ def processProduct(product, store_name, item, first):
     image_urls = product.find_elements_by_class_name('ImageLoader-image')
     if len(image_urls) != 0:
         image_url = image_urls[0].get_attribute('src')
-    # print(name)
-    try:
-    #   names = product.find_elements_by_class_name('ProductCard-name')
-    #   name = names[0].get_attribute('innerHTML')
-    #   sizes = product.find_elements_by_class_name('ProductCard-sellBy-unit')
-    #   size = sizes[0].get_attribute('innerHTML')
-    #   prices = product.find_elements_by_class_name('kds-Price')
-    #   price = prices[0].get_attribute('value')
-    #   image_urls = product.find_elements_by_class_name('ImageLoader-image')
-    #   image_url = image_urls[0].get_attribute('src')
 
+    try:
       dirsname = 'ralphs_data/' + store_name
       if not os.path.exists(dirsname):
         os.makedirs(dirsname)
 
-
+      # open the right file in the right directory for the Ralphs being scraped
       with open(dirsname + '/' + item + '.csv', 'a') as cvs:
         csv_writer_obj = csv.writer(cvs, delimiter = ',', dialect = 'excel')
         if first:
+            # Write the csv header
             csv_writer_obj.writerow(['Store', 'Product', 'Size', 'Price', 'Url', 'Category'])
         csv_writer_obj.writerow(['Ralphs_' + store_name, name, size, price, image_url, item])
-        # csv_writer_obj.writerow([Ralphs_addr, name, size, price, image_url, category])
     except Exception as e:
         print(e)
 
+# Given the search item, scrape all products that pop up for that search item.
+# For each product, get specific information about that product from the html
+# by calling processProduct. If the next page button is not present, then the scraper
+# has reached the last page for the search item and all products for that search
+# item have been scraped. If the next page button is present, click on it
+# and repeat.
 def processCategory(store_name, item):
     hasNextPage = True
     i = 0
@@ -60,6 +61,9 @@ def processCategory(store_name, item):
     while hasNextPage:
         productCard = driver.find_elements_by_class_name('ProductCard')
         for product in productCard:
+            # If first is True, then the csv header is written
+            # along with the product information in the csv
+            # (see processProduct)
             processProduct(product, store_name, item, first)
             first = False
             # TAKE THIS OUT
@@ -71,6 +75,7 @@ def processCategory(store_name, item):
         if hasNextPage:
             nextPageLink[0].click()
 
+# Given the store name, scrape all products from the SEARCH_ITEMS list.
 def processStore(store_name):
     first = True
     for item in SEARCH_ITEMS:
@@ -81,8 +86,6 @@ def processStore(store_name):
         driver.find_element_by_xpath("//button[@class='kds-Button kds-Button--primaryInverse kds-Button--hasIconOnly kds-FormField-iconButton']").click()
         processCategory(store_name, item)
         first = False
-        # TAKE THIS OUT
-        # break
 
 try:
     url = 'https://www.ralphs.com/'
@@ -108,6 +111,13 @@ try:
     seen_stores.add('13321 Jamboree Rd, Tustin, CA')
     seen_stores.add('14400 Culver Dr, Irvine, CA')
     first = True
+
+    # On the first iteration, this loop opens up the store modal and finds the names
+    # of all stores that are within DISTANCE_THRESHOLD, storing those stores in all_stores.
+    # On all other iterations, this loop finds the next store to scrape by looking through
+    # all_stores to find the next store within DISTANCE_THRESHOLD that hasn't been
+    # visited yet (not in seen_stores). It then scrapes that store, and after
+    # it is finished, adds it to seen_stores.
     while True:
         if not first:
             # open the search zip code modal
@@ -125,7 +135,7 @@ try:
         store_addresses = driver.find_elements_by_xpath("//div[@class='kds-Text--s text-default-600']")
         distance_elems = driver.find_elements_by_xpath("//div[@class='kds-Text--s text-default-600 sm:pl-0 mb-12']")
 
-        # populate stores with the store names that are valid and within distance
+        # Populate all_stores with the store names that are valid and within distance
         if first:
             for i in range(len(store_addresses)):
                 distance = distance_elems[i].get_attribute('innerHTML').split()[0]
@@ -134,6 +144,7 @@ try:
                 else:
                     all_stores.add(store_addresses[i].get_attribute('innerHTML').replace('<br>', ', '))
 
+        # Find a store in all_stores to scrape (can't be in seen_stores)
         store_index = 0
         num_stores = len(store_addresses)
         store_address = None
@@ -147,6 +158,7 @@ try:
                 found_store = True
                 break
 
+        # If found_store is False, all stores have been scraped
         if not found_store:
             break
 
@@ -155,13 +167,11 @@ try:
         store_divs[store_index].find_elements_by_xpath("//button[@class='kds-Button kds-Button--primary kds-Button--compact']")[store_index].click()
         # close the green box
         driver.find_element_by_xpath("//button[@class='kds-Toast-closeButton text-default-900 rounded-full p-0 border-none']").click()
-        # PUT THIS BACK!
-        # time.sleep(10)
         print('done')
+        # Add current store to seen_stores
         seen_stores.add(store_address)
+        # Scrape the store (see above for processStore)
         processStore(store_address)
-        # TAKE THIS OUT
-        # break
         first = False
 
 except Exception as e:
